5b2df566d3c5f6a668bf0f1f767c902145d7898f
Brett Allred <brettallred@gmail.com>
2016-04-27 23:18:35 -0600

    First cut at a feature board

--- lib/feature_board.rb
+++ lib/feature_board.rb
@@ -1,5 +1,60 @@
 require "feature_board/version"
+require "feature_board/enumerable_extensions"
+require "json"
 
 module FeatureBoard
-  # Your code goes here...
+
+  def self.features
+    @features ||= self.get_features
+  end
+
+  def self.reset_features
+    @features = self.get_features
+  end
+
+  def self.get_features
+    features = {}
+    ENV.each_pair do |key, value|
+      features[key] = value if key.end_with?("_FEATURE")
+    end
+    return features
+  end
+
+  def self.method_missing(m, *args, &block)
+    feature_name = m.to_s
+    filters = ["?", "show_", "_feature"]
+    filters.each { |filter| feature_name.gsub!(filter, "") }
+    feature_status = ENV["#{feature_name.upcase}_FEATURE"]
+    return false if feature_status.nil? 
+    self.method(feature_status.downcase).call(args.first)
+  end
+
+  def self.enabled(indentifier)
+    true
+  end
+
+  def self.whitelist(indentifier)
+    true
+  end
+
+  def self.sample10(identifier)
+    identifier.bytes.sum % 10 == 0 
+  end
+
+  def self.sample25(identifier)
+    identifier.bytes.sum % 4 == 0 
+  end
+
+  def self.sample50(identifier)
+    identifier.bytes.sum % 2 == 0 
+  end
+  
+  def self.to_json(identifier)
+    feature_set = {}
+    features.each_pair do |key, value|
+      feature_set[key] = self.method(value.downcase).call(identifier)  
+    end
+    feature_set.to_json
+  end
 end
+
--- test/feature_board_test.rb
+++ test/feature_board_test.rb
@@ -1,11 +1,67 @@
 require 'test_helper'
 
 class FeatureBoardTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::FeatureBoard::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_features_can_be_enabled_created_using_env
+    ENV["RANDOM_FEATURE"] = "enabled"
+    assert FeatureBoard.show_random?("SOME IDENTIFIER")
+    assert FeatureBoard.show_random_feature?("SOME IDENTIFIER")
   end
+
+  def test_features_are_disabled_by_default
+    refute FeatureBoard.show_unspecified_feature?("SOME IDENTIFIER")
+    refute FeatureBoard.unspecified?("SOME IDENTIFIER")
+  end
+
+  def test_features_can_be_set_as_white_list
+    ENV["WHITELIST_FEATURE"] = "whitelist"
+    assert FeatureBoard.show_whitelist_feature?("SOME IDENTIFIER")
+    assert FeatureBoard.whitelist?("SOME IDENTIFIER")
+  end
+
+  def test_features_can_be_set_as_sample10
+    ENV["SAMPLE10_FEATURE"] = "sample10"
+    assert FeatureBoard.show_sample10_feature?("d")
+    assert FeatureBoard.show_sample10_feature?("d")
+
+    refute FeatureBoard.show_sample10_feature?("a")
+    refute FeatureBoard.show_sample10_feature?("a")
+  end
+
+  def test_features_can_be_set_as_sample25
+    ENV["SAMPLE25_FEATURE"] = "sample25"
+    assert FeatureBoard.show_sample25_feature?("d")
+    assert FeatureBoard.show_sample25_feature?("d")
+
+    refute FeatureBoard.show_sample25_feature?("a")
+    refute FeatureBoard.show_sample25_feature?("a")
+  end
+
+  def test_features_can_be_set_as_sample50
+    ENV["SAMPLE50_FEATURE"] = "sample50"
+    assert FeatureBoard.show_sample50_feature?("d")
+    assert FeatureBoard.show_sample50_feature?("d")
+
+
+    refute FeatureBoard.show_sample50_feature?("a")
+    refute FeatureBoard.show_sample50_feature?("a")
+  end
+
+  def test_parses_features_by_convention
+    ENV["TEST_FEATURE"] = "enabled"
+    FeatureBoard.reset_features
+    assert_equal FeatureBoard.features["TEST_FEATURE"], "enabled"
+    assert_nil FeatureBoard.features["SOME_KEY"]
+  end
+
+  def test_to_json
+    ENV["JSON_FEATURE"] = "sample50"
+    FeatureBoard.reset_features
+    assert FeatureBoard.to_json("d").include?("\"JSON_FEATURE\":true")
+  end
+
 end
